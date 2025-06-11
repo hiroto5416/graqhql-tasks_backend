d355668bbdc269a8bb07af016569c610e7afd54b
hiroto <kitta1213@gmail.com>
2025-06-11 09:00:11 +0900

    DTOを使用した入力値バリデーション

---
+++ src/task/dto/createTask.input.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Field, InputType } from '@nestjs/graphql';
+import { IsDateString, IsNotEmpty } from 'class-validator';
+
+@InputType()
+export class CreateTaskInput {
+  @Field()
+  @IsNotEmpty()
+  name: string;
+
+  @Field()
+  @IsDateString()
+  dueDate: string;
+
+  @Field({ nullable: true })
+  description?: string;
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from './models/task.module';
+import { CreateTaskInput } from './dto/createTask.input';
 
 @Injectable()
 export class TaskService {
@@ -9,12 +10,13 @@ export class TaskService {
     return this.tasks;
   }
 
-  createTask(name: string, dueData: string, description?: string): Task {
+  createTask(createTaskInput: CreateTaskInput): Task {
+    const { name, dueDate, description } = createTaskInput;
     const newTask = new Task();
     // 現在の配列の長さプラス１の値をIDとしてセット
     newTask.id = this.tasks.length + 1;
     newTask.name = name;
-    newTask.dueData = dueData;
+    newTask.dueData = dueDate;
     newTask.status = 'NOT_STARTED';
     newTask.description = description || '';
 
